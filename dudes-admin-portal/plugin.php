15775530ad16fdf05b4b86711705efbc0ad68ba9
Jeffery Moore <jeffery.moore@scrippsnetworks.com>
2014-12-08 20:10:36 -0500

    now not allowing random dipshit hackers

--- dudes-admin-portal/plugin.php
+++ dudes-admin-portal/plugin.php
@@ -11,6 +11,15 @@
  * Network: 
  * License:
  */
+
+function check_if_logged_in() {
+	if (!is_user_logged_in() && (strpos($_SERVER['REQUEST_URI'],'wp-admin') === false) && (strpos($_SERVER['REQUEST_URI'],'wp-login') === false)) {
+		die('No content found here.');
+	}	
+}
+add_action( 'wp_loaded', 'check_if_logged_in' );
+
+
 	include('config.php');
 	include('database.php');
 	include('functions.php');
